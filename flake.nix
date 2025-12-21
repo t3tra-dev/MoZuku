ee4671e8a9183e56cf66ce73fa28e1e1757041e7
Comamoca <comamoca.dev@gmail.com>
2025-12-21 13:31:57 +0900

    :sparkles: (flake.nix, third-party/CMakeLists.txt) Add support for cross-compilation.

--- flake.nix
+++ flake.nix
@@ -42,100 +42,167 @@
           ...
         }:
         let
-          stdenv = pkgs.stdenv;
+          crossTargets = [
+            "musl64" # x86_64-linux with musl
+            "aarch64-multiplatform" # aarch64-linux
+          ];
 
-          mozuku-lsp = stdenv.mkDerivation {
-            pname = "mozuku-lsp";
-            version = "0.0.1";
-            src = ./mozuku-lsp;
-            cmakeGenerator = "Ninja";
+          mkPackagesFor =
+            targetPkgs:
+            let
+              stdenv = targetPkgs.stdenv;
 
-            nativeBuildInputs = with pkgs; [
-              mold-wrapped
-              cmake
-              ninja
-              pkg-config
-              tree-sitter
-            ];
+              mozuku-lsp = stdenv.mkDerivation {
+                pname = "mozuku-lsp";
+                version = "0.0.1";
+                src = ./mozuku-lsp;
 
-            NIX_LDFLAGS = [
-              "-fuse-ld=mold"
-            ];
+                cmakeGenerator = "Ninja";
 
-            buildInputs = with pkgs; [
-              mecab
-              crfpp
-              cabocha
-              curl
-              nlohmann_json
+                nativeBuildInputs = with pkgs; [
+                  mold-wrapped
+                  cmake
+                  ninja
+                  pkg-config
+                  tree-sitter
+                ];
 
-              tree-sitter
-              tree-sitter-grammars.tree-sitter-c
-              tree-sitter-grammars.tree-sitter-cpp
-              tree-sitter-grammars.tree-sitter-html
-              tree-sitter-grammars.tree-sitter-javascript
-              tree-sitter-grammars.tree-sitter-latex
-              tree-sitter-grammars.tree-sitter-python
-              tree-sitter-grammars.tree-sitter-rust
-              tree-sitter-grammars.tree-sitter-typescript
-              tree-sitter-grammars.tree-sitter-tsx
-            ];
-          };
+                NIX_LDFLAGS = [
+                  "-fuse-ld=mold"
+                ];
 
-          crfpp = stdenv.mkDerivation {
-            pname = "crfpp";
-            version = "0.58";
-            src = inputs.crfpp;
+                buildInputs = with targetPkgs; [
+                  mecab
+                  crfpp
+                  cabocha
+                  curl
+                  nlohmann_json
 
-            nativeBuildInputs = with pkgs; [
-              autoconf
-              automake
-              libtool
-              pkg-config
-            ];
+                  tree-sitter
+                  tree-sitter-grammars.tree-sitter-c
+                  tree-sitter-grammars.tree-sitter-cpp
+                  tree-sitter-grammars.tree-sitter-html
+                  tree-sitter-grammars.tree-sitter-javascript
+                  tree-sitter-grammars.tree-sitter-latex
+                  tree-sitter-grammars.tree-sitter-python
+                  tree-sitter-grammars.tree-sitter-rust
+                  tree-sitter-grammars.tree-sitter-typescript
+                  tree-sitter-grammars.tree-sitter-tsx
+                ];
 
-            patchPhase = ''
-              ./configure --prefix=$out
+                cmakeFlags = [
+                  "-DCABOCHA_INCLUDE_HINT=${cabocha}/include"
+                  "-DCABOCHA_LIBRARY_HINT=${cabocha}/lib"
+                  "-DCRFPP_INCLUDE_HINT=${crfpp}/include"
+                  "-DCRFPP_LIBRARY_HINT=${crfpp}/lib"
+                ];
+              };
 
-              cat > winmain.h << 'EOF'
-              // winmain.h - Empty header for Unix/Linux compatibility
-              #ifndef WINMAIN_H_
-              #define WINMAIN_H_
-              #endif
-              EOF
-            '';
+              crfpp = stdenv.mkDerivation {
+                pname = "crfpp";
+                version = "0.58";
+                src = inputs.crfpp;
 
-            enableParallelBuilding = true;
-          };
+                configurePlatforms = [
+                  "build"
+                  "host"
+                ];
 
-          cabocha = stdenv.mkDerivation {
-            pname = "cabocha";
-            version = "0.69";
-            src = inputs.cabocha;
+                nativeBuildInputs = with pkgs; [
+                  autoconf
+                  automake
+                  libtool
+                  pkg-config
+                ];
 
-            nativeBuildInputs = with pkgs; [
-              pkg-config
-              automake
-            ];
+                patchPhase = ''
+                  runHook prePatch
 
-            buildInputs = with pkgs; [
-              mecab
-              crfpp
-              libiconv
-              gettext
-            ];
+                  cat > winmain.h << 'EOF'
+                  // winmain.h - Empty header for Unix/Linux compatibility
+                  #ifndef WINMAIN_H_
+                  #define WINMAIN_H_
+                  #endif
+                  EOF
 
-            configureFlags = [
-              "--with-charset=UTF8"
-              "--with-posset=IPA"
-            ];
+                  rm -f configure
 
-            preConfigure = ''
-              cp ${pkgs.automake}/share/automake-*/install-sh .
-            '';
+                  runHook postPatch
+                '';
 
-            enableParallelBuilding = true;
-          };
+                preConfigure = ''
+                  rm -f configure
+                  autoreconf -vfi
+                '';
+
+                enableParallelBuilding = true;
+              };
+
+              cabocha = stdenv.mkDerivation {
+                pname = "cabocha";
+                version = "0.69";
+                src = inputs.cabocha;
+
+                configurePlatforms = [
+                  "build"
+                  "host"
+                ];
+
+                nativeBuildInputs = with pkgs; [
+                  pkg-config
+                  automake
+                  autoconf
+                  libtool
+                  gettext
+                ];
+
+                buildInputs = with targetPkgs; [
+                  mecab
+                  crfpp
+                  libiconv
+                ];
+
+                configureFlags = [
+                  "--with-charset=UTF8"
+                  "--with-posset=IPA"
+                ];
+
+                preConfigure = ''
+                  # install-sh がない場合のみコピー
+                  if [ ! -f install-sh ]; then
+                    cp ${pkgs.automake}/share/automake-*/install-sh .
+                  fi
+                  # configure スクリプトが存在しない場合のみ autoreconf を実行
+                  if [ ! -f configure ]; then
+                    export ACLOCAL_PATH="${pkgs.gettext}/share/aclocal:$ACLOCAL_PATH"
+                    autoreconf -vfi
+                  fi
+                '';
+
+                makeFlags =
+                  if (stdenv.buildPlatform != stdenv.hostPlatform) then
+                    [
+                      "SUBDIRS=src doc"
+                    ]
+                  else
+                    [ ];
+
+                enableParallelBuilding = true;
+              };
+            in
+            {
+              inherit mozuku-lsp crfpp cabocha;
+            };
+
+          nativePackages = mkPackagesFor pkgs;
+
+          mkMoZuKuLSP =
+            crossTarget:
+            let
+              crossPkgs = pkgs.pkgsCross.${crossTarget};
+              crossPackages = mkPackagesFor crossPkgs;
+            in
+            crossPackages.mozuku-lsp;
         in
         {
           treefmt = {
@@ -167,8 +234,8 @@
 
               # dependencies
               mecab
-              crfpp
-              cabocha
+              nativePackages.crfpp
+              nativePackages.cabocha
               nlohmann_json
 
               tree-sitter
@@ -181,10 +248,22 @@
               tree-sitter-grammars.tree-sitter-rust
               tree-sitter-grammars.tree-sitter-typescript
               tree-sitter-grammars.tree-sitter-tsx
+
+              nativePackages.crfpp
+              nativePackages.cabocha
+              nativePackages.mozuku-lsp
             ];
           };
 
-          packages.default = mozuku-lsp;
+          packages = {
+            default = nativePackages.mozuku-lsp;
+          }
+          // builtins.listToAttrs (
+            map (target: {
+              name = "mozuku-lsp-${target}";
+              value = mkMoZuKuLSP target;
+            }) crossTargets
+          );
         };
     };
 }
